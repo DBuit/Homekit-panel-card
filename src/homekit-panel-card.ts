9dacedd935f13d409ca6c000fe8fbe65a8eb756c
DBuit <daan@buit.nu>
2020-07-10 11:43:30 +0200

    Add files via upload

--- src/homekit-panel-card.ts
+++ src/homekit-panel-card.ts
@@ -1035,6 +1035,10 @@ class HomeKitCard extends LitElement {
         line-height: 35px;
         height: 35px;
       }
+
+      homekit-button.hide {
+        display:none;
+      }
       
       homekit-button .name {
         display:block;
