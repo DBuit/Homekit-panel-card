72d01859830c6b2f860997971e42433a933bf210
DBuit <daan@buit.nu>
2020-07-31 15:55:02 +0200

    Remove test title in popup, fix box-shadow in popup

--- src/homekit-panel-card.ts
+++ src/homekit-panel-card.ts
@@ -686,11 +686,11 @@ class HomeKitCard extends LitElement {
           var popUpCard = Object.assign({}, entity.popup, { entity: entity_id });
       }
       var popUpStyle = {
-        '$': ".mdc-dialog .mdc-dialog__container { width: 100%; } .mdc-dialog .mdc-dialog__container .mdc-dialog__surface { width:100%; }",
+        '$': ".mdc-dialog .mdc-dialog__container { width: 100%; } .mdc-dialog .mdc-dialog__container .mdc-dialog__surface { width:100%; box-shadow:none; }",
         '.': ":host { --mdc-theme-surface: rgba(0,0,0,0); --secondary-background-color: rgba(0,0,0,0); --ha-card-background: rgba(0,0,0,0); --mdc-dialog-scrim-color: rgba(0,0,0,0.8); --mdc-dialog-min-height: 100%; --mdc-dialog-min-width: 100%; --mdc-dialog-max-width: 100%; } mwc-icon-button { color: #FFF; }"
       }
       var service_data = {
-        title: "test",
+        title: "",
         style: popUpStyle,
         card: popUpCard,
         deviceID: ['this']
